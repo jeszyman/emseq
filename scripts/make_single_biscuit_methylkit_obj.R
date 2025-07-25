c42fb60fc940e05413abb1c430ecc31c25278ffa
jeszyman <jeszyman@gmail.com>
2025-07-25 08:01:33 -0500

    .

--- scripts/make_single_biscuit_methylkit_obj.R
+++ scripts/make_single_biscuit_methylkit_obj.R
@@ -18,3 +18,24 @@ myobj= methRead(args$bismark_cov_bed,
                 assembly= "hg38",
                 dbtype = "tabix",
                 dbdir = args$out_dir)
+
+library(argparse)
+library(methylKit)
+
+parser <- ArgumentParser()
+parser$add_argument("--bismark_cov_bed", required = TRUE)
+parser$add_argument("--library_id", required = TRUE)
+parser$add_argument("--treatment", type = "integer", required = TRUE)
+parser$add_argument("--out_dir", required = TRUE)
+
+args <- parser$parse_args()
+
+myobj= methRead(args$bismark_cov_bed,
+                sample.id = args$library_id,
+                treatment = 1,
+                context="CpG",
+                pipeline="bismarkCoverage",
+                mincov = 2,
+                assembly= "hg38",
+                dbtype = "tabix",
+                dbdir = args$out_dir)
